72c1aa69566838f550cc38323f6430284856dd96
vadim-shalapugin <vadimsalapugin@gmail.com>
2022-10-02 16:01:45 +0300

    template main menu on main page

---
+++ shop/models/menu_category.py
@@ -0,0 +1,31 @@
+from django.core.validators import FileExtensionValidator
+from django.db import models
+
+
+class MenuCategory(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Название")
+    slug = models.SlugField(verbose_name="Слаг")
+    parent = models.ForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        verbose_name="Родитель"
+    )
+    photo = models.FileField(upload_to='categories/', validators=[
+        FileExtensionValidator(
+            allowed_extensions=['img', 'svg', 'png', 'jpeg', 'ico', 'webp'],
+            message="Этот формат фотографии не подходит"
+        )], verbose_name='Фотография')
+
+    def __str__(self):
+        return f'{self.name} | Родитель - {self.parent}' if self.parent else self.name
+
+    @classmethod
+    def get_default_pk(cls):
+        obj, created = cls.objects.get_or_create(name="No category")
+        return obj.pk
+
+    class Meta:
+        verbose_name = "Категория меню"
+        verbose_name_plural = "Категории меню"
